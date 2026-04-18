6cfb80f9729c01d29203e54f24f4a22b46750210
d1000m3000-sys <d1000m3000@gmail.com>
2026-04-18 22:52:26 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
-self.addEventListener("install", () => {
-  console.log("installed");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA3o3CHKaORvCNlNpUCIU2MmaeVM6w_coo",
+  authDomain: "chatx-781cf.firebaseapp.com",
+  projectId: "chatx-781cf",
+  messagingSenderId: "339379296949",
+  appId: "1:339379296949:web:e793f99a0b9b1f16553cfc"
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "icon.png"
+  });
 });
